fe97e78a4c03c2c6b13d1823ae94ae49d71c3d8a
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-07 15:54:45 +0530

    LEET CODE - 680 -> VALID PALINDROM 2 -> returns true if the string can be made palindrome by removing one letter

--- arrays/searching_and_sorting/LECTURES/string_02.cpp
+++ arrays/searching_and_sorting/LECTURES/string_02.cpp
@@ -17,15 +17,50 @@ using namespace std;
 //     return ans;
 // }
 
-string removeDuplicateSubstring(string str, string part){
-    int pos = str.find(part);
-    while (pos != string::npos)
-    {
-        str.erase(pos, part.length());
-        pos = str.find(part);
+// string removeDuplicateSubstring(string str, string part){
+//     int pos = str.find(part);
+//     while (pos != string::npos)
+//     {
+//         str.erase(pos, part.length());
+//         pos = str.find(part);
 
+//     }
+//     return str;
+    
+// }
+
+bool checkPalindrome(string str, int i, int j){
+    while ( i <= j)
+    {
+        if ( str[i] != str[j])
+        {
+            return false;
+        }else{
+            i++;
+            j--;
+        }
     }
-    return str;
+    return true;
+    
+}
+
+bool isPalindrome(string str){
+    int i = 0;
+    int j = str.length() -1;
+
+    while ( i <= j)
+    {
+        if ( str[i] != str[j])
+        {
+            // removes either i or either j 
+            return checkPalindrome(str, i+1, j) || checkPalindrome(str, i, j-1);
+        }else{
+            i++;
+            j--;
+        }
+        
+    }
+    return true;
     
 }
 
@@ -40,16 +75,24 @@ int main() {
 
 
     // Q2 - LEET-CODE- 1910 -> REMOVE ALL OCCURENCE SUBSTRING
+    // string str;
+    // cout << " Enter the main string " << endl;
+    // cin >> str;
+    // string part;
+    // cout << " Enter the substring which has to be removed " << endl;
+    // cin >> part;
+
+    // string result = removeDuplicateSubstring(str, part);
+    // cout << "Result: " << result << endl;
+
+
+    // Q.3 - LEET - CODE - 680 -> VALID PALINDROME || -> returns true if the string can be made palindrome by removing a letter 
     string str;
-    cout << " Enter the main string " << endl;
+    cout << " Enter the string " << endl;
     cin >> str;
-    string part;
-    cout << " Enter the substring which has to be removed " << endl;
-    cin >> part;
 
-    string result = removeDuplicateSubstring(str, part);
+    bool result = isPalindrome(str);
     cout << "Result: " << result << endl;
-
 
 
 
