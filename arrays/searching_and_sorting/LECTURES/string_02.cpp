caebc7192bbdd2a25118d3bcf7fa44828fb62c68
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-07 15:12:33 +0530

    LEETCODE - 1910 -> REMOVE ALL OCCURENCE OF A SUBSTRING

--- arrays/searching_and_sorting/LECTURES/string_02.cpp
+++ arrays/searching_and_sorting/LECTURES/string_02.cpp
@@ -3,28 +3,55 @@
 
 using namespace std;
 
-string removeDuplicate(string str) {
-    string ans = "";
+// string removeDuplicate(string str) {
+//     string ans = "";
 
-    for (int i = 0; i < str.length(); i++) {
-        if (!ans.empty() && ans.back() == str[i]) {
-            ans.pop_back();
-        } else {
-            ans.push_back(str[i]);
-        }
+//     for (int i = 0; i < str.length(); i++) {
+//         if (!ans.empty() && ans.back() == str[i]) {
+//             ans.pop_back();
+//         } else {
+//             ans.push_back(str[i]);
+//         }
+//     }
+
+//     return ans;
+// }
+
+string removeDuplicateSubstring(string str, string part){
+    int pos = str.find(part);
+    while (pos != string::npos)
+    {
+        str.erase(pos, part.length());
+        pos = str.find(part);
+
     }
-
-    return ans;
+    return str;
+    
 }
 
 int main() {
-    // Q1 -> REMOVE ALL ADJACENT DUPLICATES IN STRING 
-    string str;
-    cout << "Enter the string: ";
-    cin >> str;
+    // Q1 - LEET-CODE - 1407 -> REMOVE ALL ADJACENT DUPLICATES IN STRING 
+    // string str;
+    // cout << "Enter the string: ";
+    // cin >> str;
 
-    string result = removeDuplicate(str);
+    // string result = removeDuplicate(str);
+    // cout << "Result: " << result << endl;
+
+
+    // Q2 - LEET-CODE- 1910 -> REMOVE ALL OCCURENCE SUBSTRING
+    string str;
+    cout << " Enter the main string " << endl;
+    cin >> str;
+    string part;
+    cout << " Enter the substring which has to be removed " << endl;
+    cin >> part;
+
+    string result = removeDuplicateSubstring(str, part);
     cout << "Result: " << result << endl;
+
+
+
 
     return 0;
 }
