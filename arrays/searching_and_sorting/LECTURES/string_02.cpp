9441c7785b1a1f2e52b698f81d2e2bd07c704f4a
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-09 13:47:14 +0530

    Q.5 - LEET CODE - 647 -> PALINDROMIC SUBSTRING -> find number of palindromic substring in a string

--- arrays/searching_and_sorting/LECTURES/string_02.cpp
+++ arrays/searching_and_sorting/LECTURES/string_02.cpp
@@ -31,68 +31,98 @@ using namespace std;
     
 // }
 
-bool checkPalindrome(string str, int i, int j){
-    while ( i <= j)
-    {
-        if ( str[i] != str[j])
-        {
-            return false;
-        }else{
-            i++;
-            j--;
-        }
-    }
-    return true;
+// bool checkPalindrome(string str, int i, int j){
+//     while ( i <= j)
+//     {
+//         if ( str[i] != str[j])
+//         {
+//             return false;
+//         }else{
+//             i++;
+//             j--;
+//         }
+//     }
+//     return true;
     
-}
+// }
 
-bool isPalindrome(string str){
-    int i = 0;
-    int j = str.length() -1;
+// bool isPalindrome(string str){
+//     int i = 0;
+//     int j = str.length() -1;
 
-    while ( i <= j)
-    {
-        if ( str[i] != str[j])
-        {
-            // removes either i or either j 
-            return checkPalindrome(str, i+1, j) || checkPalindrome(str, i, j-1);
-        }else{
-            i++;
-            j--;
-        }
+//     while ( i <= j)
+//     {
+//         if ( str[i] != str[j])
+//         {
+//             // removes either i or either j 
+//             return checkPalindrome(str, i+1, j) || checkPalindrome(str, i, j-1);
+//         }else{
+//             i++;
+//             j--;
+//         }
         
+//     }
+//     return true;
+    
+// }
+
+// int minimumTime(vector<string>& timePoints) {
+//     vector<int> minutes;
+
+//     // Convert each time to minutes
+//     for (int i = 0; i < timePoints.size(); i++) {
+//         string curr = timePoints[i];
+//         int hrs = stoi(curr.substr(0, 2));
+//         int min = stoi(curr.substr(3, 2));
+//         int totalMinutes = 60 * hrs + min;
+//         minutes.push_back(totalMinutes);
+//     }
+
+//     // Sort the time in minutes
+//     sort(minutes.begin(), minutes.end());
+
+//     // Find minimum difference between adjacent times
+//     int mini = INT_MAX;
+//     for (int i = 1; i < minutes.size(); i++) {
+//         int diff = minutes[i] - minutes[i - 1];
+//         mini = min(mini, diff);
+//     }
+
+//     // Check circular difference (last -> first across midnight)
+//     int circularDiff = (minutes[0] + 1440) - minutes[minutes.size() - 1];
+//     mini = min(mini, circularDiff);
+
+//     return mini;
+// }
+
+int expandIndex(string s, int i, int j){
+    int count = 0;
+    while ( i >= 0 && j < s.length() && s[i] == s[j])
+    {
+        count++;
+        i--;
+        j++;
     }
-    return true;
+    return count;
     
 }
 
-int minimumTime(vector<string>& timePoints) {
-    vector<int> minutes;
+int countPalindromicSubstring(string s){
+    int count  = 0;
+    int n = s.length();
 
-    // Convert each time to minutes
-    for (int i = 0; i < timePoints.size(); i++) {
-        string curr = timePoints[i];
-        int hrs = stoi(curr.substr(0, 2));
-        int min = stoi(curr.substr(3, 2));
-        int totalMinutes = 60 * hrs + min;
-        minutes.push_back(totalMinutes);
+    for (int i = 0; i < n; i++)
+    {
+        // for the odd string 
+        int oddAns = expandIndex(s, i, i);
+        count = count + oddAns;
+
+        // for the even string 
+        int evenAns = expandIndex(s, i , i+1);
+        count = count + evenAns;
     }
-
-    // Sort the time in minutes
-    sort(minutes.begin(), minutes.end());
-
-    // Find minimum difference between adjacent times
-    int mini = INT_MAX;
-    for (int i = 1; i < minutes.size(); i++) {
-        int diff = minutes[i] - minutes[i - 1];
-        mini = min(mini, diff);
-    }
-
-    // Check circular difference (last -> first across midnight)
-    int circularDiff = (minutes[0] + 1440) - minutes[minutes.size() - 1];
-    mini = min(mini, circularDiff);
-
-    return mini;
+    return count;
+    
 }
 
 int main() {
@@ -127,8 +157,17 @@ int main() {
 
     // Q.4 - LEET-CODE - 539 -> MINIMUM TIME DIFFERENCE
 
-    vector<string> timePoints = {"23:59", "05:00", "12:30"};
-    cout << "Minimum time difference: " << minimumTime(timePoints) << " minutes" << endl;
+    // vector<string> timePoints = {"23:59", "05:00", "12:30"};
+    // cout << "Minimum time difference: " << minimumTime(timePoints) << " minutes" << endl;
+
+    
+    // Q.5 - LEET CODE - 647 -> PALINDROMIC SUBSTRING -> find number of palindromic substring in a string 
+    string s;
+    cout << "Enter the string " << endl;
+    cin >> s;
+
+    int result = countPalindromicSubstring(s);
+    cout << " Number of palindromic substring are : " << result << endl;
     
     return 0;
 }
