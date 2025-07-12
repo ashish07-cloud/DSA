a25136175379cd4fdc75b232fe045733c4d20a04
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-12 15:04:02 +0530

    basics of pointer

--- pointer/lectures/0_1.cpp
+++ pointer/lectures/0_1.cpp
@@ -19,23 +19,36 @@ int main(){
     // access the value ptr is pointing to 
     // cout << *ptr << endl;
 
-    int a = 5;
-    int *p = &a;
+    // int a = 5;
+    // int *p = &a;
 
-    cout << sizeof(p) << endl;
+    // cout << sizeof(p) << endl;
 
-    char ch = 'b';
-    char* c  = &ch;
+    // char ch = 'b';
+    // char* c  = &ch;
 
-    cout << sizeof(c) << endl;
+    // cout << sizeof(c) << endl;
 
-    double dtr = 5.080;
-    double* d =&dtr;
+    // double dtr = 5.080;
+    // double* d =&dtr;
 
-    cout << sizeof(d) << endl;
+    // cout << sizeof(d) << endl;
 
     // no matter what type of the data is the pointer will always store the address so it will have the same size for all the data types 
-    
+
+    // NULL POINTER DECLARATION
+    // int* ptr = 0;
+    // int* ptr = nullptr;
+
+    // cout << *ptr << endl;
+
+    int a = 5;
+    int* ptr = &a;
+    int*dusraptr = ptr;
+
+    cout << *ptr << endl;
+    cout << *dusraptr << endl;
+
 
     return 0;
 }
