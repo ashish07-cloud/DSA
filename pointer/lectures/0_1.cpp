b48405aadfcbcf08763f30ad1c46d404da56e3bb
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-12 13:26:47 +0530

    basics of pointer

---
+++ pointer/lectures/0_1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int main(){
+    // int a = 5;
+    // int b = 5;
+
+    // there is a box created and a in the symbol table is created 
+    // cout << &a << endl;
+    // cout << &b << endl;
+
+    // int a = 5;
+    // pointer creation 
+    // int *ptr = &a;
+
+    // access the value ptr is pointing to 
+    // cout << *ptr << endl;
+
+    int a = 5;
+    int *p = &a;
+
+    cout << sizeof(p) << endl;
+
+    char ch = 'b';
+    char* c  = &ch;
+
+    cout << sizeof(c) << endl;
+
+    double dtr = 5.080;
+    double* d =&dtr;
+
+    cout << sizeof(d) << endl;
+
+    // no matter what type of the data is the pointer will always store the address so it will have the same size for all the data types 
+    
+
+    return 0;
+}
