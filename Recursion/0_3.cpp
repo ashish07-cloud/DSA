59fd962e6fe2c1f50d15aadbc8d0f1facfcbd197
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-14 15:08:52 +0530

    implementing binary search logic using recursion

--- Recursion/0_3.cpp
+++ Recursion/0_3.cpp
@@ -21,22 +21,47 @@ bool checkSorted(vector<int>&arr, int n, int i){
     
 }
 
+int binarySearch(vector<int>& arr, int start, int end, int target){
+    // handle the base conditions 
+    if( start > end ) return -1;
+
+    int mid = start + (end-start)/2;
+
+    if( arr[mid] == target){
+        return mid;
+    }
+
+    return arr[mid] < target ? binarySearch(arr, mid+1, end, target) : binarySearch(arr, start, mid-1, target);
+}
+
 int main(){
 
     // check wheather a array is sorted or not 
-    vector<int> arr{10, 20, 30, 40, 50, 50, 50, 20};
-    int n = arr.size();
-    int i = 0;
+    // vector<int> arr{10, 20, 30, 40, 50, 50, 50, 20};
+    // int n = arr.size();
+    // int i = 0;
 
-    bool isSorted = checkSorted(arr, n, i);
+    // bool isSorted = checkSorted(arr, n, i);
 
-    if (isSorted)
-    {
-        cout << " Array is sorted " << endl;
-    }else{
-        cout << " Array is not sorted " << endl;
-    }
+    // if (isSorted)
+    // {
+    //     cout << " Array is sorted " << endl;
+    // }else{
+    //     cout << " Array is not sorted " << endl;
+    // }
     
+
+    // applying binary search using recursion 
+    vector<int> arr{10, 20, 30, 35, 56, 78};
+    int n = arr.size();
+    int start = 0;
+    int end = n-1;
+    int target = 78;
+
+    int ans = binarySearch(arr, start, end, target);
+
+    cout << " Answer is at index " << ans << endl;
+
     
 
 
