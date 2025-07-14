2d0b4e2efb81f8c55935b043eb2dca9f47bc3bee
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2025-07-14 19:28:24 +0530

    cut a road into a given length of segments

--- Recursion/0_4.cpp
+++ Recursion/0_4.cpp
@@ -25,32 +25,59 @@ using namespace std;
 
 
 // this is the second method to do the same but here we will take a output which is intialized to 0 and then after we go on increasing until the target is achieved 
-int solve(vector<int>& arr, int target, int output) {
-    // Base cases
-    if (output == target) return 0;
-    if (output > target) return INT_MAX;
+// int solve(vector<int>& arr, int target, int output) {
+//     // Base cases
+//     if (output == target) return 0;
+//     if (output > target) return INT_MAX;
 
-    int mini = INT_MAX;
-    for (int i = 0; i < arr.size(); i++) {
-        int res = solve(arr, target, output + arr[i]);
-        if (res != INT_MAX) {
-            mini = min(mini, res + 1);  // Include this coin
-        }
-    }
+//     int mini = INT_MAX;
+//     for (int i = 0; i < arr.size(); i++) {
+//         int res = solve(arr, target, output + arr[i]);
+//         if (res != INT_MAX) {
+//             mini = min(mini, res + 1);  // Include this coin
+//         }
+//     }
 
-    return mini;
+//     return mini;
+// }
+
+int solve(int n, int x, int y, int z){
+    // base case 
+    if( n == 0) return 0;
+    if (n < 0) return INT_MIN;
+
+    int ans1 = solve(n-x, x, y, z) + 1; // here +1 is done in all the answers because the solve function return ans for the value n-x
+    int ans2 = solve(n-y, x, y, z) + 1;
+    int ans3 = solve(n-z, x, y, z) + 1;
+
+    int ans = max(ans1, max(ans2, ans3));
+    return ans;
+
+
 }
 
 
 int main () {
 
-    vector<int> arr{2, 3};
-    int target = 5;
-    int output = 0;
+    // vector<int> arr{2, 3};
+    // int target = 5;
+    // int output = 0;
 
-    int ans = solve(arr, target, output);
+    // int ans = solve(arr, target, output);
 
-    cout << "answer is " << ans << endl;
+    // cout << "answer is " << ans << endl;
+
+    int n = 7, x = 5, y = 2, z = 2;
+
+    int ans = solve(n, x, y, z);
+
+    if(ans < 0){
+        ans = 0;
+    }
+    cout << " Answer is " << ans << endl;
+
+
+
 
 
     return 0;
